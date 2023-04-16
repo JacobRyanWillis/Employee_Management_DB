8f7777e95f25d8fd92678832503dad0ef41a8d5e
JacobWillis <jacobwillis2012@gmail.com>
2023-04-16 09:09:04 -0400

    'updated schema and seeds'

--- Develop/db/seeds.sql
+++ Develop/db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
     ('John', 'Doe', 1, NULL),
     ('Jane', 'Doe', 2, 1),
